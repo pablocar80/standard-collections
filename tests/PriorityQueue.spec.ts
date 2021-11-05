bc0473fef0eb45c883af108d30fc161c39dc161a
Pablo Carbonell <pablocar80@gmail.com>
2021-11-05 16:34:43 -0500

    bump version

--- tests/PriorityQueue.spec.ts
+++ tests/PriorityQueue.spec.ts
@@ -2,12 +2,12 @@ import { PriorityQueue } from "../src/priority-queue";
 
 test("simple priority queue", () => {
     const q = new PriorityQueue<number>();
-    q.insert(50);
-    q.insert(30);
-    q.insert(40);
-    expect(q.extract()).toBe(30);
-    expect(q.extract()).toBe(40);
-    expect(q.extract()).toBe(50);
+    q.insert(10);
+    q.insert(4);
+    q.insert(8);
+    expect(q.extract()).toBe(4);
+    expect(q.extract()).toBe(8);
+    expect(q.extract()).toBe(10);
     expect(q.size()).toBe(0);
     expect(q.isEmpty()).toBeTruthy();
 });
